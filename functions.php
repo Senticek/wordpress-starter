0b04df486186f023362552a5908b6b8767f2f433
Tomas <tomas.svoboda111@gmail.com>
2021-02-23 20:15:52 +0100

    script register test

--- functions.php
+++ functions.php
@@ -29,13 +29,13 @@ add_action('wp_enqueue_scripts', 'load_stylesheets');
 function addjs()
 {
 
-    wp_register_script('jqueryM', get_template_directory_uri() . 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js', array(), 1 , 1, 1);
+    wp_register_script('jqueryM', get_template_directory_uri() . "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js", array(), 1 , 1, 1);
     wp_enqueue_script('jqueryM');
 
-    wp_register_script('bootstrap', get_template_directory_uri() . 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', array(), 1 , 1, 1);
+    wp_register_script('bootstrap', get_template_directory_uri() . "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js", array(), 1 , 1, 1);
     wp_enqueue_script('bootstrap');
 
-    wp_register_script('jqueryE', get_template_directory_uri() . 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js', array(), 1 , 1, 1);
+    wp_register_script('jqueryE', get_template_directory_uri() . "https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js", array(), 1 , 1, 1);
     wp_enqueue_script('jqueryE');
 
     wp_register_script('main', get_template_directory_uri() . 'js/main.js', array(), 1 , 1, 1);
@@ -43,11 +43,7 @@ function addjs()
 
 
 }
+add_action('wp_enqueue_scripts', 'addjs');
 
 
-/*<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
-<!-- Third party plugin JS-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-<!-- Core theme JS-->
-<script src="js/main.js"></script>*/
+
