45014a8583a224def4ef505245449936a9e1fd82
Tomas <tomas.svoboda111@gmail.com>
2021-02-28 16:06:57 +0100

    Contact form 7 add and basic form remove

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_template_part('includes/header');?>
     <!-- Portfolio Section-->
-    <?php $main = get_field('basic');?>
+
     <section class="page-section portfolio" id="portfolio">
         <div class="container">
             <!-- Portfolio Section Heading-->
@@ -124,51 +124,12 @@
             <!-- Contact Section Form-->
             <div class="row">
                 <div class="col-lg-8 mx-auto">
-                    <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                <label>Jméno</label>
-                                <input class="form-control" id="name" type="text" placeholder="Jméno"
-                                    required="required" data-validation-required-message="Please enter your name." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                <label>Email</label>
-                                <input class="form-control" id="email" type="email" placeholder="Email"
-                                    required="required"
-                                    data-validation-required-message="Please enter your email address." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                <label>Telefon</label>
-                                <input class="form-control" id="phone" type="tel" placeholder="Telefon"
-                                    required="required"
-                                    data-validation-required-message="Please enter your phone number." />
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <div class="control-group">
-                            <div class="form-group floating-label-form-group controls mb-0 pb-2">
-                                <label>Vaše zpráva</label>
-                                <textarea class="form-control" id="message" rows="5" placeholder="Vaše zpráva"
-                                    required="required"
-                                    data-validation-required-message="Please enter a message."></textarea>
-                                <p class="help-block text-danger"></p>
-                            </div>
-                        </div>
-                        <br />
-                        <div id="success"></div>
-                        <div class="form-group"><button class="btn btn-primary btn-xl" id="sendMessageButton"
-                                type="submit">Odeslat</button></div>
-                    </form>
+                <?php echo do_shortcode('[contact-form-7 id="87" title="Form"]')?> 
                 </div>
             </div>
         </div>
     </section>
+   
    
 
 <?php get_template_part('includes/footer');?>
