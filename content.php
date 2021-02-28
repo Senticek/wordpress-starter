6c5b7d8469182b78e2f8d1495b89dc07d2997a67
Tomas <tomas.svoboda111@gmail.com>
2021-02-28 16:31:41 +0100

    typo

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <?php /* Template Name: Content */ ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php get_template_part('includes/header'); ?>
+<?php get_template_part('includes/header'); ?>
         <main>
             <h1><?php the_title() ?></h1>
             <?php the_content(); ?>
